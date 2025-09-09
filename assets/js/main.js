5d25578cc92786cce360a3dc17771709de471675
tommy lease <tommy@ingrooves.com>
2025-09-08 22:58:35 -0700

    plenty of changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,13 @@
 		$main = $('#main');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+		// breakpoints({
+		// 	xlarge:   [ '1281px',  '1680px' ],
+		// 	large:    [ '981px',   '1280px' ],
+		// 	medium:   [ '737px',   '980px'  ],
+		// 	small:    [ '481px',   '736px'  ],
+		// 	xsmall:   [ null,      '480px'  ]
+		// });
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
@@ -78,18 +78,6 @@
 				window.setTimeout(function() {
 					$search.removeClass('visible');
 				}, 100);
-			});
-
-	// Intro.
-		var $intro = $('#intro');
-
-		// Move to main on <=large, back to sidebar on >large.
-			breakpoints.on('<=large', function() {
-				$intro.prependTo($main);
-			});
-
-			breakpoints.on('>large', function() {
-				$intro.prependTo($sidebar);
 			});
 
 })(jQuery);
